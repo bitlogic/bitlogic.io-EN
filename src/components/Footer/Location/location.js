1f2bb582fb324356abdd671970da3da213c2a531
GonzalezMaxi <maximiliano.gonzalez@bitlogic.io>
2024-07-25 09:25:01 -0300

    SBIT-348: modify structure and styles to Footer

--- src/components/Footer/Location/location.js
+++ src/components/Footer/Location/location.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React from "react"
 import FaIcon from "../../FaIcon/FaIcon"
-import "./location.scss";
+import "./location.scss"
+import PropTypes from "prop-types"
 
-export default function Location({ location }) {
-  const locationItems = location?.iconText.map((item, index) => {
+export default function Location({ locationData }) {
+  if (!locationData) return null
+
+  const location = locationData?.iconText?.map(item => {
     return (
-      <div className="icon-text d-flex" key={`${item.name}-${index}`}>
-        {item.icon && <FaIcon type={item.icon.type} code={item.icon.code} />}
+      <div className="icon-text d-flex" key={item.id}>
+        <FaIcon type={item.icon.type} code={item.icon.code} />
         {item.name}
       </div>
     )
-  });
+  })
+
   return (
-    <div className='ContactData__Item'>
-      <h6>{location?.title}</h6>
-      <div className='ContactData__Item__location'>{locationItems}</div>
+    <div className="Footer__location">
+      <h6>{locationData?.title}</h6>
+      <div className="Footer__location__items">{location}</div>
     </div>
   )
+}
+
+Location.propTypes = {
+  locationData: PropTypes.shape({
+    title: PropTypes.string,
+    iconText: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string.isRequired,
+        icon: PropTypes.shape({
+          type: PropTypes.string.isRequired,
+          code: PropTypes.string.isRequired,
+        }),
+      })
+    ),
+  }),
 }
