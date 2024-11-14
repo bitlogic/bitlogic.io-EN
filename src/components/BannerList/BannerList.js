cfe44a5d7d6fd49feb32cd14abed3f5a75d5978e
Flor <florencia.schroder@bitlogic.io>
2024-11-14 13:11:45 -0300

    no se mostraban los professionals

--- src/components/BannerList/BannerList.js
+++ src/components/BannerList/BannerList.js
@@ -58,7 +58,7 @@ BannerList.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
     strapi_component: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
     contactForm: PropTypes.bool,
     concactFormAnchor: PropTypes.string,
     callToAction: PropTypes.string,
